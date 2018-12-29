51c5c28df0385859d9c74750a44d16ad7f7586ba
Mark Kopec <mark.kopec@gmail.com>
2018-12-29 18:54:28 -0500

    Complete first section of passport

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,7 +2,10 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const User = require('../../models/User');
+const keys = require('../../config/keys')
+const jwt = require('jsonwebtoken');
 
+// these are the routes
 router.post('/register', (req, res) => {
   // Check to make sure nobody has already registered with a duplicate email
   User.findOne({ email: req.body.email })
@@ -28,6 +31,47 @@ router.post('/register', (req, res) => {
           })
         })
       }
+    })
+})
+
+// another route
+router.post('/login', (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // pass to mongoose a request to find the user, it returns a promise
+  User.findOne({email})
+    .then(user => {
+      if (!user) {
+        return res.status(404).json( {email: "This user does not exist"});
+      }
+
+      bcrypt.compare(password, user.password)
+        .then(isMatch => {
+          if (isMatch) {
+            const payload = { 
+              id: user.id, 
+              handle: user.handle, 
+              email: user.email 
+            };
+
+            jwt.sign(
+              payload,
+              keys.secretOrKey,
+              // Tell the key to expire in one hour
+              { expiresIn: 3600 },
+              (err, token) => {
+                res.json({
+                  success: true,
+                  token: 'Bearer ' + token
+                });
+              });
+          } else {
+            return res
+              .status(400)
+              .json({ password: "Incorrect password" });
+          }
+        })
     })
 })
 

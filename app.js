d0ded0b218fdee2c32ffb929e396f9bf7b465baa
Mark Kopec <mark.kopec@gmail.com>
2019-01-01 23:57:23 -0800

    Add links to heroku

--- app.js
+++ app.js
@@ -5,8 +5,16 @@ const tweets = require("./routes/api/tweets");
 const bodyParser = require('body-parser');
 const User = require('./models/User')
 const passport = require("passport");
+const path = require("path");
 
 const app = express();
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 app.use(passport.initialize());
 require("./config/passport")(passport);
